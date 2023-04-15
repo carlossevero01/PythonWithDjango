1ce5ecab03d47e17dd549cc01d50c3cbe25d840c
carlossevero01 <carlos212427@gmail.com>
2023-04-15 18:05:31 -0300

    AplicaçãoDjango

---
+++ sistema/authusuarios/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AuthusuariosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'authusuarios'
---
+++ sistema/eventos/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Evento, Certificado
+
+admin.site.register(Evento)
+admin.site.register(Certificado)
+# Register your models here.
